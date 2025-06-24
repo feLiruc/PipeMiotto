a3ef141237508218c0163aa477e2f58915cd1897
feLiruc <fim1991@hotmail.com>
2025-06-24 13:52:59 -0300

    Primeiro commit

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const { init, insertEvent, insertFullLog } = require('./db');
 require('dotenv').config();
 
 const app = express();
+app.use(express.json());
+
 const PORT = process.env.PORT || 3000;
 
 app.post('/webhook', async (req, res) => {
